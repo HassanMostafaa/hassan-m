25b5f5cd6f602733c0608fb969871e91f2c0afdf
HassanMostafaa <hassan.mohamed@evolvextechnology.com>
2026-09-21 15:50:27 +0400

    init header setup - testing deployed database

--- server/types/generated/components.d.ts
+++ server/types/generated/components.d.ts
@@ -1,3 +1,34 @@
-/*
- * The app doesn't have any components yet.
- */
+import type { Schema, Struct } from '@strapi/strapi';
+
+export interface BaseComponentsButton extends Struct.ComponentSchema {
+  collectionName: 'components_base_components_buttons';
+  info: {
+    displayName: 'Button';
+  };
+  attributes: {
+    ButtonText: Schema.Attribute.String &
+      Schema.Attribute.Required &
+      Schema.Attribute.DefaultTo<'Button'>;
+    ButtonUrl: Schema.Attribute.String;
+  };
+}
+
+export interface HeaderComponenetsLink extends Struct.ComponentSchema {
+  collectionName: 'components_header_componenets_links';
+  info: {
+    displayName: 'Link';
+  };
+  attributes: {
+    linkText: Schema.Attribute.String;
+    LinkUrl: Schema.Attribute.String;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export namespace Public {
+    export interface ComponentSchemas {
+      'base-components.button': BaseComponentsButton;
+      'header-componenets.link': HeaderComponenetsLink;
+    }
+  }
+}
--- server/types/generated/contentTypes.d.ts
+++ server/types/generated/contentTypes.d.ts
@@ -480,6 +480,35 @@ export interface AdminUser extends Struct.CollectionTypeSchema {
   };
 }
 
+export interface ApiHeaderHeader extends Struct.SingleTypeSchema {
+  collectionName: 'headers';
+  info: {
+    displayName: 'Header';
+    pluralName: 'headers';
+    singularName: 'header';
+  };
+  options: {
+    draftAndPublish: true;
+  };
+  attributes: {
+    Btns: Schema.Attribute.Component<'base-components.button', false>;
+    createdAt: Schema.Attribute.DateTime;
+    createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
+      Schema.Attribute.Private;
+    Links: Schema.Attribute.Component<'header-componenets.link', true>;
+    locale: Schema.Attribute.String & Schema.Attribute.Private;
+    localizations: Schema.Attribute.Relation<
+      'oneToMany',
+      'api::header.header'
+    > &
+      Schema.Attribute.Private;
+    publishedAt: Schema.Attribute.DateTime;
+    updatedAt: Schema.Attribute.DateTime;
+    updatedBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
+      Schema.Attribute.Private;
+  };
+}
+
 export interface PluginContentReleasesRelease
   extends Struct.CollectionTypeSchema {
   collectionName: 'strapi_releases';
@@ -992,6 +1021,7 @@ declare module '@strapi/strapi' {
       'admin::transfer-token': AdminTransferToken;
       'admin::transfer-token-permission': AdminTransferTokenPermission;
       'admin::user': AdminUser;
+      'api::header.header': ApiHeaderHeader;
       'plugin::content-releases.release': PluginContentReleasesRelease;
       'plugin::content-releases.release-action': PluginContentReleasesReleaseAction;
       'plugin::i18n.locale': PluginI18NLocale;
