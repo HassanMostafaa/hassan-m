07f4de9bc543cb351cdfd715d2a0bf454c9af4f3
HassanMostafaa <hassan.mohamed@evolvextechnology.com>
2026-09-22 11:11:56 +0400

    setup prod env codegen config

--- client/codegen.ts
+++ client/codegen.ts
@@ -2,7 +2,7 @@ import "dotenv/config";
 import type { CodegenConfig } from "@graphql-codegen/cli";
 
 const config: CodegenConfig = {
-  schema: `${process.env.API_URL}/graphql`,
+  schema: `${process.env.API_URL === "development" ? process.env.API_URL : process.env.PROD_API_URL}/graphql`,
   documents: "src/services/graphql/**/*.ts",
   generates: {
     "src/types/IGenTypes.ts": {
